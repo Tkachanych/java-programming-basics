b640cfe85d2da115226aaa9b2d667bd3a408c873
Vladimir Tkachenko <81316027+Tkachanych@users.noreply.github.com>
2025-04-28 18:20:53 +0400

    First commit

---
+++ src/main/java/org/javaguru/student/lesson_1_software_setup/helloworld.java
@@ -0,0 +1,10 @@
+package org.javaguru.student.lesson_1_software_setup;
+
+class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Valera");
+        System.out.println("Kovs");
+    }
+
+}
